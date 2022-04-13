bd198f6fb25f8c12ed038b1e355b34665a67e157
midgjs <sjqksk.10@gmail.com>
2022-04-13 16:51:35 +0900

    22-04-13

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,61 @@
+/*
+작성자: 이재훈
+작성일: 2022-04-13
+설 명: 나이기록 프로그램
+*/
+
+package com.javaex.ex01;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		int myAge = 25; //변수선언+초기화
+		
+		/*
+		int myAge; //변수선언
+		myAge = 25; //초기화
+		*/
+		
+		//메모리의 값을 출력하는 메소드
+		System.out.println(myAge);
+		
+		myAge = 26;
+		System.out.println(myAge);
+		
+		
+		byte no;
+		no = -128;
+		System.out.println(no);
+		
+		/*int 범위까지는 L을 붙이지 않아도 된다.*/
+		long no2;
+		no2 = 9223123123L;
+		System.out.println(no2);
+		
+		////////////////////////
+		//변수선언, 초기화
+		////////////////////////
+		
+		//변수여러개일때 변수선언, 초기화
+		int var01;
+		int var02;
+		int var03;
+		
+		//int var01, var02, var03;
+		
+		var01 = 10;
+		var02 = 20;
+		var03 = 30;
+		
+		//변수여러개일때 변수선언+초기화
+		int var04 = 10;
+		int var05 = 20;
+		int var06 = 30;
+		
+		//int var04 = 10, int var05 =20, int var06 = 30;
+		
+		System.out.println(var04);
+	}
+	
+}

d4a0209a561b5346d5310aa8cf3707b7bfdedf95
midgjs <sjqksk.10@gmail.com>
2022-04-21 17:43:43 +0900

    2022-04-21

---
+++ src/com/javaex/ex05/Ex08.java
@@ -0,0 +1,16 @@
+package com.javaex.ex05;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<args.length; i++){
+			System.out.println(args[i]);
+			}
+
+		
+		
+	//cmd 창에서 배열 확인하는 프로그램	
+	}
+
+}
